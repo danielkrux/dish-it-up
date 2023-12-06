a4f1d1c3825cb8fc943b2890eeab8afaf8f05805
Daniel Krux <daniel@passionatepeople.io>
2023-12-06 15:08:36 +0100

    Fix indentation in useRefreshOnFocus hook

--- src/hooks/useRefreshOnFocus.ts
+++ src/hooks/useRefreshOnFocus.ts
@@ -2,15 +2,15 @@ import { useFocusEffect } from "@react-navigation/native";
 import * as React from "react";
 
 export function useRefreshOnFocus(refetch: () => void) {
-	const enabledRef = React.useRef(false);
+  const enabledRef = React.useRef(false);
 
-	useFocusEffect(
-		React.useCallback(() => {
-			if (enabledRef.current) {
-				refetch();
-			} else {
-				enabledRef.current = true;
-			}
-		}, [refetch]),
-	);
+  useFocusEffect(
+    React.useCallback(() => {
+      if (enabledRef.current) {
+        refetch();
+      } else {
+        enabledRef.current = true;
+      }
+    }, [refetch, enabledRef]),
+  );
 }
