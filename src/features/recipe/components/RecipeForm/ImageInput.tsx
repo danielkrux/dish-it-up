0a847228ba874233aad2ac969b6d706fd84cd43a
danielkrux <danielmartijn@gmail.com>
2024-01-09 19:26:04 +0100

    remove log

--- src/features/recipe/components/RecipeForm/ImageInput.tsx
+++ src/features/recipe/components/RecipeForm/ImageInput.tsx
@@ -17,7 +17,6 @@ export type ImageInputProps = {
 };
 
 function ImageInput({ initialImages = [], onChange }: ImageInputProps) {
-	console.log(initialImages);
 	const [images, setImages] = useState<
 		{
 			base64: string;
