a88cfd4b1cce85712a16d5112cce61f80b57263b
danielkrux <danielmartijn@gmail.com>
2024-04-14 12:02:46 +0200

    add nativewind types

--- src/features/recipe/components/RecipeDetail/RecipeDetail.tsx
+++ src/features/recipe/components/RecipeDetail/RecipeDetail.tsx
@@ -14,84 +14,85 @@ import Instructions from "./Instructions";
 import Meta from "./Meta";
 
 export default function RecipeDetail({
-	id,
-	header,
-	logRecipeRef,
+  id,
+  header,
+  logRecipeRef,
 }: {
-	id: number;
-	header?: ReactNode;
-	logRecipeRef: React.RefObject<BottomSheetModalMethods>;
+  id: number;
+  header?: ReactNode;
+  logRecipeRef: React.RefObject<BottomSheetModalMethods>;
 }) {
-	const { data } = useFetchRecipe(id);
-	const { containerSize, onLayout } = useContainerBreakpoint();
+  const { data } = useFetchRecipe(id);
+  const { containerSize, onLayout } = useContainerBreakpoint();
 
-	if (containerSize === "md") {
-		return (
-			<View className="px-20 flex-1">
-				<View className="flex-1 flex-row g-10 mb-10">
-					<ScrollView contentContainerStyle="pt-5" className="flex-1">
-						<Image
-							source={data?.images?.[0]}
-							className="aspect-square rounded-2xl mb-4"
-						/>
-						<Text className="mb-4 font-display text-xl">{data?.name}</Text>
-						<ChipList
-							className="mb-2"
-							data={data?.categories.map((c) => ({
-								value: String(c.id),
-								label: c.name,
-							}))}
-						/>
-						{data?.description && (
-							<Text className="mb-4 max-w-screen-sm">{data?.description}</Text>
-						)}
-						<Meta recipe={data} className="mb-4" />
-					</ScrollView>
-					<ScrollView contentContainerStyle="pt-5" className="flex-1">
-						<Instructions recipe={data} className="mx-4 mb-5" />
-					</ScrollView>
-					<ScrollView contentContainerStyle="pt-5" className="flex-1">
-						<Ingredients recipe={data} className="mb-2 mx-4" />
-					</ScrollView>
-				</View>
-			</View>
-		);
-	}
+  if (containerSize === "md") {
+    return (
+      <View className="px-20 pb- flex-1">
+        <View className="flex-1 flex-row g-10 mb-10">
+          <ScrollView contentContainerStyle="pt-5" className="flex-1">
+            <Image
+              source={data?.images?.[0]}
+              className="aspect-square rounded-2xl mb-4"
+            />
+            <Text className="mb-4 font-display text-xl">{data?.name}</Text>
+            <ChipList
+              className="mb-2"
+              data={data?.categories.map((c) => ({
+                value: String(c.id),
+                label: c.name,
+              }))}
+            />
+            {data?.description && (
+              <Text className="mb-4 max-w-screen-sm">{data?.description}</Text>
+            )}
+            <Meta recipe={data} className="mb-4" />
+          </ScrollView>
+          <ScrollView contentContainerStyle="pt-5" className="flex-1">
+            <Instructions recipe={data} className="mx-4 mb-5" />
+          </ScrollView>
+          <ScrollView contentContainerStyle="pt-5" className="flex-1">
+            <Ingredients recipe={data} className="mb-2 mx-4" />
+          </ScrollView>
+        </View>
+      </View>
+    );
+  }
 
-	return (
-		<ScrollView
-			onLayout={onLayout}
-			contentInsetAdjustmentBehavior="automatic"
-			className="flex-1 py-4 px-4"
-		>
-			{header}
-			<View className="mb-4">
-				<View className="flex-row justify-between items-end mb-4 g-4">
-					<Text className="font-display text-4xl">{data?.name}</Text>
-					<StarRating
-						onPress={() => logRecipeRef?.current?.present()}
-						initialValue={data?.rating}
-						short
-						className="mb-1.5"
-					/>
-				</View>
+  return (
+    <ScrollView
+      onLayout={onLayout}
+      contentInsetAdjustmentBehavior="automatic"
+      className="flex-1 pt-4 px-4"
+      contentContainerStyle="pb-20"
+    >
+      {header}
+      <View className="mb-4">
+        <View className="flex-row justify-between items-end mb-4 g-4">
+          <Text className="font-display text-4xl">{data?.name}</Text>
+          <StarRating
+            onPress={() => logRecipeRef?.current?.present()}
+            initialValue={data?.rating}
+            short
+            className="mb-1.5"
+          />
+        </View>
 
-				<Image
-					source={data?.images?.[0]}
-					className="aspect-video rounded-2xl"
-				/>
-			</View>
-			{data?.description && <Text className="mb-4">{data?.description}</Text>}
-			<Meta recipe={data} className="mb-5" />
-			<ChipList
-				className="mb-5"
-				data={data?.categories.map((c) => ({
-					value: String(c.id),
-					label: c.name,
-				}))}
-			/>
-			<Ingredients recipe={data} className="mb-7" />
-			<Instructions recipe={data} className="mb-5" />
-		</ScrollView>
-	);
+        <Image
+          source={data?.images?.[0]}
+          className="aspect-video rounded-2xl"
+        />
+      </View>
+      {data?.description && <Text className="mb-4">{data?.description}</Text>}
+      <Meta recipe={data} className="mb-5" />
+      <ChipList
+        className="mb-5"
+        data={data?.categories.map((c) => ({
+          value: String(c.id),
+          label: c.name,
+        }))}
+      />
+      <Ingredients recipe={data} className="mb-7" />
+      <Instructions recipe={data} className="mb-5" />
+    </ScrollView>
+  );
 }
