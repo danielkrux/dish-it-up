0728afb8abb5a963499dc1d9e27b3d33ca7726c6
danielkrux <danielmartijn@gmail.com>
2024-04-22 11:37:49 +0200

    categories empty state

--- src/app/(app)/settings/categories.tsx
+++ src/app/(app)/settings/categories.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { Alert, FlatList, StyleSheet, View } from "react-native";
 import FloatingButton from "~/components/FloatingButton";
+import Icon from "~/components/Icon";
 import IconButton from "~/components/IconButton";
 import Text from "~/components/Text";
 import { CATEGORIES_QUERY_KEY } from "~/features/app/app.constants";
@@ -63,29 +64,41 @@ export default function Settings() {
 
   return (
     <>
-      <FlatList
-        data={data}
-        ItemSeparatorComponent={() => (
-          <View className="h-[1] border-b-gray-100 dark:border-b-gray-900 border-b" />
-        )}
-        renderItem={({ item: category }) => (
-          <View key={category.id} style={styles.category}>
-            <Text>{category.name}</Text>
-            <View style={styles.actions}>
-              <IconButton
-                onPress={() => updateCategory(category.id, category.name)}
-                icon="Pencil"
-                size="medium"
-              />
-              <IconButton
-                onPress={() => deleteCategory(category.id)}
-                icon="Trash2"
-                size="medium"
-              />
+      {data?.length ? (
+        <FlatList
+          data={data}
+          ItemSeparatorComponent={() => (
+            <View className="h-[1] border-b-gray-100 dark:border-b-gray-900 border-b" />
+          )}
+          renderItem={({ item: category }) => (
+            <View key={category.id} style={styles.category}>
+              <Text>{category.name}</Text>
+              <View style={styles.actions}>
+                <IconButton
+                  onPress={() => updateCategory(category.id, category.name)}
+                  icon="Pencil"
+                  size="medium"
+                />
+                <IconButton
+                  onPress={() => deleteCategory(category.id)}
+                  icon="Trash2"
+                  size="medium"
+                />
+              </View>
             </View>
-          </View>
-        )}
-      />
+          )}
+        />
+      ) : (
+        <View className="bg-gray-100 dark:bg-gray-900 rounded-lg mx-3 p-10 py-12 mt-12 items-center">
+          <Icon className="mb-5" name="Tag" size={64} />
+          <Text type="header" size="l" className="text-center mb-2">
+            You don't have any categories yet!
+          </Text>
+          <Text className="text-center">
+            You can create a category with the button below.
+          </Text>
+        </View>
+      )}
       <FloatingButton useSafeArea onPress={addCategory}>
         Add Category
       </FloatingButton>
