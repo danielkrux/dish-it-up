1d0de1cfb2bf31dd80ffe43e8570c47acb541195
danielkrux <danielmartijn@gmail.com>
2024-04-22 10:24:34 +0200

    empty state recipe history

--- src/app/(app)/(tabs)/account.tsx
+++ src/app/(app)/(tabs)/account.tsx
@@ -3,6 +3,7 @@ import { Image } from "expo-image";
 import { useRouter } from "expo-router";
 import React from "react";
 import { Pressable, View } from "react-native";
+import Icon from "~/components/Icon";
 
 import Text from "~/components/Text";
 import recipeKeys from "~/features/recipe/recipe.queryKeys";
@@ -11,44 +12,49 @@ import useAuth from "~/hooks/useAuth";
 import { formatDistanceToNowInDays } from "~/utils/date";
 
 function Account() {
-	const router = useRouter();
-	const { session } = useAuth();
-	const { data } = useQuery(recipeKeys.lastMade(), getLastMadeRecipes);
+  const router = useRouter();
+  const { session } = useAuth();
+  const { data } = useQuery(recipeKeys.lastMade(), getLastMadeRecipes);
 
-	return (
-		<View className="mx-4">
-			<Text className="mb-8">{session?.user.email}</Text>
+  return (
+    <View className="mx-4">
+      <Text className="mb-8">{session?.user.email}</Text>
 
-			<View className="flex-row justify-between">
-				<Text className="font-display text-xl mb-2">Last Made</Text>
-				{/* <Button variant="ghost">See all</Button> */}
-			</View>
-			<View className=" bg-gray-100 dark:bg-gray-900 rounded-xl p-4 g-4">
-				{data?.map((r) => (
-					<Pressable
-						onPress={() => router.push(`/recipe/${r.id}/`)}
-						className="flex-row g-3"
-						key={r.id}
-					>
-						{r.images?.length ? (
-							<Image className="w-20 h-20 rounded-xl" source={r.images[0]} />
-						) : null}
-						<View className="flex-1">
-							<Text numberOfLines={2} className="font-display text-base">
-								{r?.name}
-							</Text>
-							<Text
-								numberOfLines={1}
-								className="font-body text-gray-500 dark:text-gray-300 text-sm"
-							>
-								{formatDistanceToNowInDays(new Date(r?.last_cooked ?? ""))}
-							</Text>
-						</View>
-					</Pressable>
-				))}
-			</View>
-		</View>
-	);
+      <View className="flex-row justify-between">
+        <Text className="font-display text-xl mb-2">Last Made</Text>
+        {/* <Button variant="ghost">See all</Button> */}
+      </View>
+      <View className=" bg-gray-100 dark:bg-gray-900 rounded-xl p-4 g-4">
+        {data?.map((r) => (
+          <Pressable
+            onPress={() => router.push(`/recipe/${r.id}/`)}
+            className="flex-row g-3"
+            key={r.id}
+          >
+            {r.images?.length ? (
+              <Image className="w-20 h-20 rounded-xl" source={r.images[0]} />
+            ) : null}
+            <View className="flex-1">
+              <Text numberOfLines={2} className="font-display text-base">
+                {r?.name}
+              </Text>
+              <Text
+                numberOfLines={1}
+                className="font-body text-gray-500 dark:text-gray-300 text-sm"
+              >
+                {formatDistanceToNowInDays(new Date(r?.last_cooked ?? ""))}
+              </Text>
+            </View>
+          </Pressable>
+        ))}
+        {data?.length === 0 && (
+          <Text className="text-gray-300 text-center">
+            You haven't made any recipes yet!
+          </Text>
+        )}
+      </View>
+    </View>
+  );
 }
 
 export default Account;
