5d69aacc74e19a1d325ff47461fa6733d2cc3201
danielkrux <danielmartijn@gmail.com>
2024-05-26 11:42:15 +0200

    bump version

--- app.config.js
+++ app.config.js
@@ -47,7 +47,7 @@ module.exports = {
     favicon: "./assets/favicon.png",
   },
   ios: {
-    buildNumber: "8",
+    buildNumber: "9",
     supportsTablet: true,
     bundleIdentifier: "com.danielkrux.dishitup",
     associatedDomains: [
