75030e00f744e62f6b2838043a6aac5dfb871d33
danielkrux <danielmartijn@gmail.com>
2024-06-19 12:49:17 +0200

    fix bug in crash

--- src/features/recipe/hooks/useFetchRecipes.ts
+++ src/features/recipe/hooks/useFetchRecipes.ts
@@ -1,17 +1,18 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import recipeKeys from "../recipe.queryKeys";
 import { getRecipeByIds } from "../recipe.service";
-import { Recipe } from "../recipe.types";
+import type { Recipe } from "../recipe.types";
 
 function useFetchRecipes(ids?: number[]) {
   const queryClient = useQueryClient();
 
-  const recipes = queryClient.getQueryData<Recipe[]>(recipeKeys.all, {
-    exact: false,
-  });
+  const recipes = queryClient.getQueryData<Recipe[]>(recipeKeys.all);
 
   const result = useQuery(recipeKeys.mealPlan(), () => getRecipeByIds(ids), {
-    initialData: () => recipes?.filter((recipe) => ids?.includes(recipe.id)),
+    initialData: () => {
+      if (!recipes) return;
+      return recipes?.filter((recipe) => ids?.includes(recipe.id));
+    },
   });
 
   return result;
